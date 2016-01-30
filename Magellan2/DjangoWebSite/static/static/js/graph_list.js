1a92bef61b33aebc2e991c4cbfb3a9cc4edef327
Pawan Darda <pawan.darda@gmail.com>
2016-01-30 12:27:16 +0800

    deleting links

--- Magellan2/DjangoWebSite/static/static/js/graph_list.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$(".tabs_versions").tabs();
-	$("span.ver_header").mbFlipText(false);
-	$(".shortcut").mouseenter(function() { $(this).addClass('ui-state-hover');})
-	$(".shortcut").mouseleave(function() { $(this).removeClass("ui-state-hover");})
-});
