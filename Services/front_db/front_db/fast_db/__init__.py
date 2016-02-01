ea72d136a2aff0a3edf98a79116c3a6d5fae764a
Pawan Darda <pawan.darda@gmail.com>
2016-02-01 20:33:47 +0800

    Python module '__init__' added to repo front-src

---
+++ Services/front_db/front_db/fast_db/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: latin-1 -*-
+# Injecting the fast dialect code into the sqlalchemy dialects package.
+
+import direct_wrapper, meta_wrapper
+
+#import sys
+#from sqlalchemy import dialects
+#from front_db.fast_db import fast
+#dialects.fast = fast
+#sys.modules.setdefault('sqlalchemy.dialects.fast',fast)
